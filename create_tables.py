d322999484a6a51d7704481b2112334c6c5510be
kateryna-golovko <kateryna.d.golovko@gmail.com>
2025-02-22 13:58:26 +1000

    Added docstrings to create_tables.py and etl.py.

--- create_tables.py
+++ create_tables.py
@@ -2,32 +2,41 @@ import configparser # Read configuration file
 import psycopg2 # PostgreSQL adopter to interact with Redshift
 from sql_queries import create_table_queries, drop_table_queries # Import queries for tables drop and creation
 
-# The function that drops the existing tables to avoid errors if re-run of the script is required
 def drop_tables(cur, conn):
+    """
+    The function that drops the existing tables to avoid errors if re-run of the script is required
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
-# The function that creates empty staging and final tables with defined column names and types
 def create_tables(cur, conn):
+    """
+    The function that creates empty staging and final tables with defined column names and types
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
-# The main function that runs the table creation process
 def main():
-    # Read the config file that contains the actual values for the Redshift cluster, IAM role, etc.
+    """
+    The main function that runs the table creation process:
+        - Read the config file that contains the actual values for the Redshift cluster, IAM role, etc.
+        - Extract values from the config file: cluster is cluster`s endpoint, but without the ':' and stuff after it, e.g. no ':5439/dev'
+        - Establish connection to Redshift
+        - Create a cursor to interact with the database
+        - Call the functions created above 
+        - Close the connection
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    # Extract values from the config file
-    host = config['CLUSTER']['HOST'] # cluster is cluster`s endpoint, but without the ':' and stuff after it, e.g. no ':5439/dev'
+    host = config['CLUSTER']['HOST'] 
     dbname = config['CLUSTER']['DB_NAME']
     user = config['CLUSTER']['DB_USER']
     password = config['CLUSTER']['DB_PASSWORD']
     port = config['CLUSTER']['DB_PORT']
     
-    # Establish connection to Redshift
     conn = psycopg2.connect(
         host=host,
         dbname=dbname,
@@ -36,21 +45,11 @@ def main():
         port=port
     )
     
-    # Debug if needed - if errors occur during connection, check whether everything is entered correctly
-    #print(f"Host: {host}")
-    #print(f"DB Name: {dbname}")
-    #print(f"User: {user}")
-    #print(f"Password: {password}")
-    #print(f"Port: {port}")
-    
-    # Create a cursor to interact with the database
     cur = conn.cursor()
 
-    # Call the functions created above 
     drop_tables(cur, conn)
     create_tables(cur, conn)
 
-    # Close the connection
     conn.close()
 
 # When the script is being executed (e.g. using python create_tables.py), run the main function
--- etl.py
+++ etl.py
@@ -2,10 +2,14 @@ import configparser # Read configuration file
 import psycopg2 # PostgreSQL adopter to interact with Redshift
 from sql_queries import copy_table_queries, insert_table_queries # Import copy and insert queries for later execution
 
-# The function to load data from the S3 bucket into the earlier created staging tables
-# Added the config input during debugging 
 def load_staging_tables(cur, conn, config):
-    # For each copy query, replace the variable names with the actual values from the config file
+    """
+    The function to load data from the S3 bucket into the earlier created staging tables:
+        - Added the config input during debugging 
+        - For each copy query, replace the variable names with the actual values from the config file
+        - Execute the above query to load the data into the staging tables
+        - Commit the transaction to Redshift
+    """
     for query in copy_table_queries:
         query = query.format(
             log_data=config['S3']['LOG_DATA'], 
@@ -13,31 +17,39 @@ def load_staging_tables(cur, conn, config):
             log_jsonpath=config['S3']['LOG_JSONPATH'],
             song_data=config['S3']['SONG_DATA']
         )
-        # Execute the above query to load the data into the staging tables
+        
         cur.execute(query)
-        # Commit the transaction to Redshift
         conn.commit()
 
-# The function to insert the data from the staging tables into the final tables
 def insert_tables(cur, conn):
+    """
+    The function to insert the data from the staging tables into the final tables
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
-# The main function that runs the ETL pipeline 
 def main():
-    # Read the config file that contains the actual values for the Redshift cluster, IAM role, etc.
+    """
+    The main function that runs the ETL pipeline:
+        - Read the config file that contains the actual values for the Redshift cluster, IAM role, etc.
+        - Extract values from the config file
+        - Establish connection to Redshift,
+        - Create a cursor to interact with the database
+        - Call the functions created above:
+            - Load the data from S3 into the staging tables
+            - Insert the data from the staging tables into the final tables
+        - Close the connection
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    # Extract values from the config file
     host = config['CLUSTER']['HOST'] 
     dbname = config['CLUSTER']['DB_NAME']
     user = config['CLUSTER']['DB_USER']
     password = config['CLUSTER']['DB_PASSWORD']
     port = config['CLUSTER']['DB_PORT']
     
-    # Establish connection to Redshift
     conn = psycopg2.connect(
         host=host,
         dbname=dbname,
@@ -46,14 +58,11 @@ def main():
         port=port
     )
     
-    # Create a cursor to interact with the database
     cur = conn.cursor()
     
-    # Call the functions created above 
-    load_staging_tables(cur, conn, config) # Load the data from S3 into the staging tables
-    insert_tables(cur, conn) #Insert the data from the staging tables into the final tables
+    load_staging_tables(cur, conn, config) 
+    insert_tables(cur, conn) 
     
-    # Close the connection
     conn.close()
 
 # When the script is being executed (e.g. using python etl.py), run the main function
